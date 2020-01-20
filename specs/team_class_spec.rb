dd94de24b8315aae18a6ffcbfb4ddec3d51519fc
danyibalazs <danyi.balage@gmail.com>
2020-01-20 14:10:59 +0000

    Team class added

--- specs/team_class_spec.rb
+++ specs/team_class_spec.rb
@@ -5,5 +5,6 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 class TestTeamClass < MiniTest::Test
   def setup
+    @team = Team.new("Rangers", ["Bob", "John"], "Frank")
   end
 end
---
+++ team_class.rb
@@ -0,0 +1,7 @@
+class Team
+  def initialize(team_name, players_array, coach_name)
+    @team_name = team_name
+    @players_array = players_array
+    @coach_name = coach_name
+  end
+end
